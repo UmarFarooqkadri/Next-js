4b25543c854f56bd9289542bc750ae6983cdbce0
Umar Farooq <36752798+UmarFarooqkadri@users.noreply.github.com>
2024-08-01 13:50:25 +0500

    Update quicksort.cpp

---
+++ quicksort.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+using namespace std;
+
+
+void combine(int arr[],si,mid,eni){
+    int merged[]=new int[eni-si+1];
+    int indx1=si;
+    int indx2=mid+1;
+    int x=0;
+
+    while(indx1<=mid && indx2<=eni){
+        if(arr[indx1]<=arr[indx2]){
+            merged[x]=arr[indx1];
+            indx1++;
+            x++;
+        }else{
+            merged[x]=arr[indx2];
+            indx2++;
+            x++;
+        }
+    }
+    while(indx1<=mid){
+        merged[x]=arr[indx1];
+        indx1++;
+        x++;
+    }
+    while(indx2<=eni){
+        merged[x]=arr[indx2];
+        indx2++;
+        x++;
+    }
+  for(int i=0,j=0; i<eni-si+1; i++,j++){
+      arr[i]=merged[j];
+  }
+    
+}
+
+void divide(int arr[],int si,int eni){
+    if(si>=eni){
+        return
+    }
+    else{
+        int mid=(si+eni)/2;
+        divide(arr,si,mid);
+        divide(arr,mid+1,eni);
+        combine(arr,si,mid,eni);
+    }
+}
+
+int main(){
+    int arr[] = {5, 4, 3, 2, 1};
+
+    int n=arr.length();
+    cout<<n<<endl;
+    //divide(arr,0,n-1);
+
+}
